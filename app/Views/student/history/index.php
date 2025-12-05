16ee4546b78316640650dce550e8fd43fa45dbff
AjiNurAji <ajinuraji090306@gmail.com>
2025-12-05 20:37:33 +0700

    fix: table responsive

--- app/Views/student/history/index.php
+++ app/Views/student/history/index.php
@@ -2,68 +2,70 @@
 <?php include __DIR__ . "/../../layout/dashboard/top.php"; ?>
 
 <div class="container mx-auto">
-  <div class="overflow-hidden bg-white p-4 rounded-lg shadow">
-    <div class="overflow-x-auto relative w-full">
-      <table class="w-full text-sm text-left rtl:text-right text-body" id="myTable">
-        <thead class="text-sm text-body bg-neutral-secondary-medium border-b border-t border-default-medium">
-          <tr>
-            <th scope="col" class="px-6 py-3 font-medium">
-              No
-            </th>
-            <th scope="col" class="px-6 py-3 font-medium">
-              Nama Lengkap
-            </th>
-            <th scope="col" class="px-6 py-3 font-medium">
-              Kelas
-            </th>
-            <th scope="col" class="px-6 py-3 font-medium">
-              Mata Kuliah
-            </th>
-            <th scope="col" class="px-6 py-3 font-medium">
-              Masuk
-            </th>
-            <th scope="col" class="px-6 py-3 font-medium">
-              Pulang
-            </th>
-            <th scope="col" class="px-6 py-3 font-medium">
-              Status
-            </th>
-            <th scope="col" class="px-6 py-3 font-medium">
-              Catatan
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($attendances as $i => $value): ?>
-            <tr class="bg-neutral-primary-soft border-b border-default hover:bg-neutral-secondary-medium">
-              <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
-                <?= htmlspecialchars($i + 1) ?>
-              </td>
-              <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
-                <?= htmlspecialchars($value->student_name) ?>
-              </td>
-              <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
-                <?= htmlspecialchars($value->class_name) ?>
-              </td>
-              <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
-                <?= htmlspecialchars($value->course_name) ?>
-              </td>
-              <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
-                <?= htmlspecialchars(toIDTime($value->in_time)) ?>
-              </td>
-              <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
-                <?= htmlspecialchars( $value->out_time ?  toIDTime($value->out_time) : "") ?>
-              </td>
-              <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
-                <?= htmlspecialchars($value->status === "present" ? "Hadir" : "Tidak Hadir") ?>
-              </td>
-              <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
-                <?= htmlspecialchars($value->note) ?>
-              </td>
+  <div class="bg-white p-4 rounded-lg shadow">
+    <div class="overflow-auto">
+      <div class="overflow-x-auto relative w-full">
+        <table class="w-full text-sm text-left rtl:text-right text-body" id="myTable">
+          <thead class="text-sm text-body bg-neutral-secondary-medium border-b border-t border-default-medium">
+            <tr>
+              <th scope="col" class="px-6 py-3 font-medium">
+                No
+              </th>
+              <th scope="col" class="px-6 py-3 font-medium">
+                Nama Lengkap
+              </th>
+              <th scope="col" class="px-6 py-3 font-medium">
+                Kelas
+              </th>
+              <th scope="col" class="px-6 py-3 font-medium">
+                Mata Kuliah
+              </th>
+              <th scope="col" class="px-6 py-3 font-medium">
+                Masuk
+              </th>
+              <th scope="col" class="px-6 py-3 font-medium">
+                Pulang
+              </th>
+              <th scope="col" class="px-6 py-3 font-medium">
+                Status
+              </th>
+              <th scope="col" class="px-6 py-3 font-medium">
+                Catatan
+              </th>
             </tr>
-          <?php endforeach ?>
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            <?php foreach ($attendances as $i => $value): ?>
+              <tr class="bg-neutral-primary-soft border-b border-default hover:bg-neutral-secondary-medium">
+                <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                  <?= htmlspecialchars($i + 1) ?>
+                </td>
+                <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                  <?= htmlspecialchars($value->student_name) ?>
+                </td>
+                <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                  <?= htmlspecialchars($value->class_name) ?>
+                </td>
+                <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                  <?= htmlspecialchars($value->course_name) ?>
+                </td>
+                <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                  <?= htmlspecialchars(toIDTime($value->in_time)) ?>
+                </td>
+                <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                  <?= htmlspecialchars( $value->out_time ?  toIDTime($value->out_time) : "") ?>
+                </td>
+                <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                  <?= htmlspecialchars($value->status === "present" ? "Hadir" : "Tidak Hadir") ?>
+                </td>
+                <td class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                  <?= htmlspecialchars($value->note) ?>
+                </td>
+              </tr>
+            <?php endforeach ?>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </div>
